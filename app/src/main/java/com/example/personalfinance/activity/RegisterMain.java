9e33bbe3aaabab77e56705567605a04f8e9bce56
Tran Tan Phuoc <trantanphuoc262@gmail.com>
2022-05-14 23:52:51 +0700

    TranTanPhuoc_firstcommit

---
+++ app/src/main/java/com/example/personalfinance/activity/RegisterMain.java
@@ -0,0 +1,15 @@
+package com.example.personalfinance.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.personalfinance.R;
+
+import android.os.Bundle;
+
+public class RegisterMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_main);
+    }
+}
